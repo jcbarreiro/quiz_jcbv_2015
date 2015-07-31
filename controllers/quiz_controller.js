f8718829b7ef00121b839f96b518f7e306932c51
jcbarreiro <juancarlos.barreirovazquez@gmail.com>
2015-07-31 21:04:39 +0200

    commit 31/07/2015 21:04

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,10 +13,28 @@ exports.load = function(req, res, next, quizId) {
 };
 
 // GET /quizes
+//exports.index = function(req, res) {
+//	models.Quiz.findAll().then (function(quizes) {
+//		res.render('quizes/index.ejs', {quizes: quizes});
+//	})
+//};
+// GET /quizes
 exports.index = function(req, res) {
-	models.Quiz.findAll().then (function(quizes) {
-		res.render('quizes/index.ejs', {quizes: quizes});
-	})
+  if(req.query.search !== undefined){  
+    var palabras = req.query.search.split(' ')
+    var frase_filtrada = palabras.join("%");
+    models.Quiz.findAll({where: ["pregunta like ?", '%' + frase_filtrada + '%' ]}).then(
+      function(quizes) {
+        res.render('quizes/index', { quizes: quizes, errors: []});
+      }
+    ).catch(function(error) { next(error);})
+  }else{
+    models.Quiz.findAll().then(
+      function(quizes) {
+        res.render('quizes/index', { quizes: quizes, errors: []});
+      }
+    ).catch(function(error) { next(error);})
+  }
 };
 
 // GET /quizes/question
